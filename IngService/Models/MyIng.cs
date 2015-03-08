478e13b8d9cf467ebf3e5f792caefc605ad9c1ed
grj1046 <guorenjun@outlook.com>
2015-03-08 21:16:38 +0800

    move IngType MyIng to Models folder

--- IngService/Models/MyIng.cs
+++ IngService/Models/MyIng.cs
@@ -1,10 +1,9 @@
-﻿using IngService.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace IngService.Services
+namespace IngService.Models
 {
     /// <summary>
     /// 我的闪存
